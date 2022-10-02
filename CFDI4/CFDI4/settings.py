a22f226c9961ea46ae366a6cb320699162ab5303
Francisco Rodriguez <franciscojavierrod28@gmail.com>
2022-10-01 19:48:20 -0600

    Mysql connection settings changes

--- CFDI4/CFDI4/settings.py
+++ CFDI4/CFDI4/settings.py
@@ -86,17 +86,28 @@ WSGI_APPLICATION = 'CFDI4.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'CFDI4',
-        'USER': 'sa',
-        'PASSWORD': 'Soluciones28',
-        'HOST': 'ASJUA-WEB05\SQLEXPRESS',
-        'PORT': '',
-        'OPTIONS': {
-            'driver': 'SQL Server Native Client 11.0',
-        },
-    },
+    
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cfdi4',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+    
+    }
+    # 'default': {
+    #     'ENGINE': 'sql_server.pyodbc',
+    #     'NAME': 'CFDI4',
+    #     'USER': 'sa',
+    #     'PASSWORD': 'Soluciones28',
+    #     'HOST': 'ASJUA-WEB05\SQLEXPRESS',
+    #     'PORT': '',
+    #     'OPTIONS': {
+    #         'driver': 'SQL Server Native Client 11.0',
+    #     },
+    # },
 }  
 
 
