0e08e46571217d2bee45ff9bcc9081841caea192
Francisco Rodriguez <franciscojavierrod28@gmail.com>
2022-10-15 18:04:40 -0600

    models import delete innecesary

--- CFDI4/Profiles/models.py
+++ CFDI4/Profiles/models.py
@@ -1,9 +1,3 @@
-from email.mime import image
-from email.policy import default
-from pyexpat import model
-from random import choices
-from turtle import right
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 
