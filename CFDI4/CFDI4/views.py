d4d84df7cf19d05a856f60bf4e10c1b2f19f5661
Francisco Rodriguez <franciscojavierrod28@gmail.com>
2022-10-23 17:08:04 -0600

    login logout changes

--- CFDI4/CFDI4/views.py
+++ CFDI4/CFDI4/views.py
@@ -15,13 +15,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from .forms import SignupForm
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    # form_class = UserCreationForm
+    form_class = SignupForm
     success_url = reverse_lazy("index")
     template_name = "Profiles/signup.html"
-
+    
 #Class based view to register user
 class RegisterUserAPIView(generics.CreateAPIView):
     
