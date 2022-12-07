6d11d6fc8f444e06ac5598d4d24a2f0ffb81a6aa
Francisco Rodriguez <franciscojavierrod28@gmail.com>
2022-12-06 22:06:22 -0700

    CSSConfig signal

--- CFDI4/Profiles/signals.py
+++ CFDI4/Profiles/signals.py
@@ -1,4 +1,5 @@
 from .models import Profile
+from Config.models import CSSConf
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -7,7 +8,11 @@ from django.dispatch import receiver
 def post_save_create_profile(sender, instance, created, *args, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        CSSConf.objects.create(user=instance)
+        
         
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):    
-    instance.profile.save()
+def save_profile(sender,instance,created, **kwargs):
+    instance.profile.save()    
+    if not created:        
+        instance.user_confCSS.get_or_create(user=instance)
